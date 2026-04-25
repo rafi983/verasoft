9f2a217b7247e5adc570bd0a108e1efd796f0b12
rafi zaman <rafiirfan211@gmail.com>
2026-04-25 15:54:35 +0600

    delete .next folder

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"v/ccVZeEhGF9nI+jMxEVjvj/8vgjKeS8eNUNPOZiYDI=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0dd_qpv._.js","static/chunks/_0~lai3e._.js","static/chunks/[root-of-the-server]__0sz5qe-._.css"],
-    source: "dynamic"
-});
